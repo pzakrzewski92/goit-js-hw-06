10db37184fa6a3d2ac17aed9a88050e74ea02086
Piotr Zakrzewski <p.zakrzewski92@wp.pl>
2022-09-21 22:37:00 +0200

    update task 1

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const categories = document.querySelector('categories.length');
+console.log(categories);
+
+categories.forEach(function (number, li.item) {
+    console.log(`<h2> ${li.item}, wartość ${number}</h2>`);
+  });
+
