9a95a6127ba2d065a67a2f519182bd9df61a0700
Piotr Zakrzewski <p.zakrzewski92@wp.pl>
2022-09-23 18:46:03 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
 const categories = document.querySelectorAll('.item');
-console.log(categories.length);
+console.log(`Number of categories: ${categories.length}`);
 
 categories.forEach(category => {
-  category.querySelectorAll("li")
+  console.log(`Category: ${category.querySelector.innerText("h2")}`);
 });
 
