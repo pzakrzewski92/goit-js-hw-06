1ff9fea8d73c2bd0ecdffcaed05ec0053a1c216b
Piotr Zakrzewski <p.zakrzewski92@wp.pl>
2022-09-24 14:26:13 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,7 +1,27 @@
 let startValue = document.querySelector('#value')
 let counterValue = parseInt(startValue.innerHTML)
 let button = document.querySelectorAll('#counter button')
-counterValue.forEach(counter => {
-    console.log(`Category: ${category.querySelector("h2").innerText}`);
-  console.log(`Elements: ${category.querySelectorAll('li').length}`)
-  });
+
+button.forEach(el =>  {
+    el.addEventListener("click", (event) => {
+        event.preventDefault();
+        const{
+            element: button
+        } = event.currentTarget;
+        console.log(button.value);
+    });
+//     if (dataset.action === "decrement") {
+//         counterValue = counterValue - 1;
+//     }
+//     counterValue = counterValue + 1;
+//     })  
+
+//     const form = document.querySelector(".register-form");
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const {
+//     elements: { username, password }
+//   } = event.currentTarget;
+//   console.log(username.value, password.value);
+// });
