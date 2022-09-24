76d672c82f38adefb4b203add3eb608872a4d05c
Piotr Zakrzewski <p.zakrzewski92@wp.pl>
2022-09-24 16:09:26 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,27 +1,27 @@
-let startValue = document.querySelector('#value')
-let counterValue = parseInt(startValue.innerHTML)
-let button = document.querySelectorAll('#counter button')
-
-button.forEach(el =>  {
-    el.addEventListener("click", (event) => {
-        event.preventDefault();
-        const{
-            element: button
-        } = event.currentTarget;
-        console.log(button.value);
-    });
-//     if (dataset.action === "decrement") {
-//         counterValue = counterValue - 1;
-//     }
-//     counterValue = counterValue + 1;
-//     })  
-
-//     const form = document.querySelector(".register-form");
-
-// form.addEventListener("submit", (event) => {
-//   event.preventDefault();
-//   const {
-//     elements: { username, password }
-//   } = event.currentTarget;
-//   console.log(username.value, password.value);
+const sum = document.querySelector("#value");
+const buttons = document.querySelectorAll("#counter button");
+let currentValue = parseInt(sum.innerHTML);
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.dataset.action === "decrement"
+      ? currentValue--
+      : button.dataset.action === "increment"
+      ? currentValue++
+      : "";
+    sum.innerHTML = currentValue;
+  });
+});
+//-----------------------
+// const sum = document.querySelector("#value");
+// const buttons = document.querySelectorAll("#counter button");
+// let currentValue = parseInt(sum.innerHTML);
+// buttons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//    if(button.dataset.action === "decrement"){
+//       currentValue--
+//    }else if(button.dataset.action === "increment"){
+//       currentValue++
+//    }
+//     sum.innerHTML = currentValue;
+//   });
 // });
