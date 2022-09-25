b6d6a68fab71f265c891703b49ebdf81a7cdbbcf
Piotr Zakrzewski <p.zakrzewski92@wp.pl>
2022-09-25 17:08:30 +0200

    update js 07

--- js/task-06.js
+++ js/task-06.js
@@ -8,7 +8,7 @@ textInput.addEventListener("blur", (event) => {
     if (textInput.length <= 6) {
         event.target.style.border = 'textInput.valid';
     } else {
-        event.target.style.border = '#validation-input.invalid';
+        event.target.style.border = 'document.#validation-input.invalid';
     }
   
 });
--- js/task-07.js
+++ js/task-07.js
@@ -2,5 +2,5 @@ const textInput = document.querySelector("#font-size-control");
 const output = document.querySelector("#text");
 
 textInput.addEventListener("input", (event) => {
-  output.textContent = event.currentTarget.value;
+    output.style.fontSize = `${event.target.value}px`;
 });
