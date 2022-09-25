1a964ccadde8d095afb36b0d3c8fb966a93295df
Piotr Zakrzewski <p.zakrzewski92@wp.pl>
2022-09-25 17:56:09 +0200

    update mentor

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,10 @@
 const textInput = document.querySelector("#validation-input");
 
-// textInput.addEventListener("focus", () => {
-//   textInput.value = "This input has focus";
-// });
-
 textInput.addEventListener("blur", (event) => {
-    if (textInput.length <= 6) {
-        event.target.style.border = 'document.#validation-input.valid';
+    if (textInput.length <= `${document.input.data-length}`) {
+        element.style.border = 'document.#validation-input.valid';
     } else {
-        event.target.style.border = 'document.#validation-input.invalid';
+        element.style.border = 'document.#validation-input.invalid';
     }
   
 });
