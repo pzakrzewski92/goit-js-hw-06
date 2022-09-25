1d4ced33875a15d5dba69053d469d61fa34cf6fd
Piotr Zakrzewski <p.zakrzewski92@wp.pl>
2022-09-25 20:22:58 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,34 @@
-const textInput = document.querySelector("#validation-input");
+// const textInput = document.querySelector("#validation-input");
 
-textInput.addEventListener("blur", (event) => {
-    if (textInput.length <= `${document.input.data-length}`) {
-        $('#validation-input').addClass('validation-input.valid');
-    } else {
-        $('#validation-input').addClass('validation-input.invalid');
-    }
+// textInput.addEventListener("blur", (event) => {
+//     if (textInput.length >= `${document.input.data-length}`) {
+//         $('#validation-input').addClass('validation-input.valid');
+//     } else {
+//         $('#validation-input').addClass('validation-input.invalid');
+//     }
   
-});
+// });
+
+// const inputEl = document.querySelector('#validation-input');
+// const inputLengthEl = Number(inputEl.dataset.length);
+// inputEl.addEventListener('blur', () => {
+//     if (inputEl.value.length === inputLengthEl) {
+//         inputEl.classList.add('valid');
+//         inputEl.classList.remove('invalid');
+//     } else {
+//         inputEl.classList.add('invalid');
+//         inputEl.classList.remove('valid');
+//     }
+// }
+// );
+
+const inputnameEl = document.querySelector('#validation-input');
+console.log(inputnameEl.placeholder);
+const validLength = parseInt(inputnameEl.getAttribute("data-length"))
+console.log(validLength);
+inputnameEl.addEventListener('blur', (event) =>
+{if (event.currentTarget.value.length === validLength)
+    {event.currentTarget.classList.add('valid')
+    event.currentTarget.classList.remove('invalid')}
+    else {event.currentTarget.classList.add('invalid')
+event.currentTarget.classList.remove('valid')}});
