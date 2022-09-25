474f89d8e398a0ce0c77c24c9426437be1f9225c
Piotr Zakrzewski <p.zakrzewski92@wp.pl>
2022-09-25 20:47:40 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyBackground = document.querySelector("body");
+const changeBtn = bodyBackground.querySelector(".change-color")
+const color = bodyBackground.querySelector(".color")
+
+const changeBgColor = () => {
+  bodyBackground.style.backgroundColor = `${getRandomHexColor()}`;
+  color.textContent = `${getRandomHexColor()}`;
+};
+
+changeBtn.addEventListener("click", changeBgColor);
