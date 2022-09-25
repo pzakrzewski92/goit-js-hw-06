ca556d848ababb82f54597f283320f1158b1a2ea
Piotr Zakrzewski <p.zakrzewski92@wp.pl>
2022-09-25 18:00:53 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -9,9 +9,9 @@ function handleSubmit(event) {
   } = event.currentTarget;
 
   if (email.value === "" || password.value === "") {
-    return console.log("Please fill in all the fields!");
+    return alert("Please fill in all the fields!");
   }
 
-  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
   event.currentTarget.reset();
 }
