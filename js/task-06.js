088d673aac8b4c388271620544298b390e52193c
Piotr Zakrzewski <p.zakrzewski92@wp.pl>
2022-09-25 19:41:26 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,9 +2,9 @@ const textInput = document.querySelector("#validation-input");
 
 textInput.addEventListener("blur", (event) => {
     if (textInput.length <= `${document.input.data-length}`) {
-        element.style.border = 'document.#validation-input.valid';
+        $('#validation-input').addClass('validation-input.valid');
     } else {
-        element.style.border = 'document.#validation-input.invalid';
+        $('#validation-input').addClass('validation-input.invalid');
     }
   
 });
